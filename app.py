bd3df9e9b08480453ad3d6f0485acd1deab25c8b
kyb3r <abdurraqeeb53@gmail.com>
2019-05-13 21:20:16 +1000

    Redirect to where you logged in from

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os 
 from functools import wraps
-from urllib.parse import urlencode
+from urllib.parse import urlencode, urlparse
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from sanic import Sanic, response
@@ -11,7 +11,7 @@ from jinja2 import Environment, PackageLoader
 import aiohttp
 
 from core.models import LogEntry
-from core.utils import get_stack_variable, authrequired
+from core.utils import get_stack_variable, authrequired, User
 
 OAUTH2_CLIENT_ID = os.getenv('OAUTH2_CLIENT_ID')
 OAUTH2_CLIENT_SECRET = os.getenv('OAUTH2_CLIENT_SECRET')
@@ -24,6 +24,7 @@ ROLE_URL = API_BASE + '/guilds/{guild_id}/members/{user_id}'
 
 app = Sanic(__name__)
 app.using_oauth = (OAUTH2_CLIENT_ID and OAUTH2_CLIENT_SECRET)
+app.bot_id = OAUTH2_CLIENT_ID
 
 Session(app, interface=InMemorySessionInterface())
 app.static('/static', './static')
@@ -45,10 +46,11 @@ app.render_template = render_template
 @app.listener('before_server_start')
 async def init(app, loop):
     app.db = AsyncIOMotorClient(os.getenv('MONGO_URI')).modmail_bot
-    app.session = aiohttp.ClientSession()
+    app.session = aiohttp.ClientSession(loop=loop)
     if app.using_oauth:
         app.guild_id = os.getenv('GUILD_ID')
         app.bot_token = os.getenv('TOKEN')
+        app.netloc = urlparse(OAUTH2_REDIRECT_URI).netloc
         print('USING OAUTH2 MODE')
 
 async def fetch_token(code):
@@ -60,17 +62,19 @@ async def fetch_token(code):
         "client_secret": OAUTH2_CLIENT_SECRET,
         "scope": "identify"
     }
+
+    headers = {'Content-Type': 'x-www-form-urlencoded'}
     
     print(data)
 
-    async with app.session.post(TOKEN_URL, params=data) as resp:
+    async with app.session.post(TOKEN_URL, data=data) as resp:
         json = await resp.json()
         print(json)
         return json
 
 async def get_user_info(token):
     headers = {"Authorization": f"Bearer {token}"}
-    async with app.session.get(f"{API_BASE_URL}/users/@me", headers=headers) as resp:
+    async with app.session.get(f"{API_BASE}/users/@me", headers=headers) as resp:
         return await resp.json()
 
 async def get_user_roles(user_id):
@@ -78,24 +82,36 @@ async def get_user_roles(user_id):
     headers = {'Authorization': f'Bot {app.bot_token}'}
     async with app.session.get(url, headers=headers) as resp:
         user = await resp.json()
+        print(user)
     return user.get('roles', [])
+
+app.get_user_roles = get_user_roles
 
 @app.exception(NotFound)
 async def not_found(request, exc):
     return render_template('not_found')
 
 @app.exception(Unauthorized)
-async def not_found(request, exc):
-    return render_template('unauthorized')
-
+async def not_authorized(request, exc):
+    return render_template(
+        'unauthorized', 
+        message='You do not have permission to view this page.'
+        )
 
 @app.get('/')
 async def index(request):
     return render_template('index')
 
-
 @app.get('/login')
 async def login(request):
+    referer = request.headers.get('referer', '/')
+    
+    if referer != '/' and urlparse(referer).netloc != app.netloc:
+        referer = '/' # dont redirect to a different site
+    request['session']['referer'] = referer
+
+    print(request['session'])
+
     data = {
         "scope": "identify",
         "client_id": OAUTH2_CLIENT_ID,
@@ -109,6 +125,8 @@ async def login(request):
 async def oauth_callback(request):
     if request.args.get('error'):
         return response.redirect('/login')
+
+    print(request['session'])
         
     code = request.args.get('code')
     token = await fetch_token(code)
@@ -116,9 +134,16 @@ async def oauth_callback(request):
     if access_token is not None:
         request['session']['access_token'] = access_token
         request['session']['logged_in'] = True
-        request['session']['user'] = await get_user_info(access_token)
-        return response.redirect('/')
+        request['session']['user'] = User(await get_user_info(access_token))
+        print(request['session']['user'])
+        return response.redirect(request['session']['referer'])
     return response.redirect('/login')
+
+
+@app.get('/logout') 
+async def logout(request):
+    request['session'].clear()
+    return response.redirect('/')
 
 @app.get('/logs/raw/<key>')
 @authrequired()
@@ -126,7 +151,7 @@ async def get_raw_logs_file(request, key):
     document = await app.db.logs.find_one({'key': key})
 
     if document is None:
-        return response.text('Not Found', status=404)
+        abort(404)
 
     log_entry = LogEntry(app, document)
 
@@ -141,16 +166,11 @@ async def get_logs_file(request, key):
     document = await app.db.logs.find_one({'key': key})
 
     if document is None:
-        return response.text('Not Found', status=404)
+        abort(404)
 
     log_entry = LogEntry(app, document)
 
     return log_entry.render_html()
-
-
-@app.get('/favicon.ico')
-async def get_favicon(request):
-    return await response.file('/static/favicon.ico')
 
 if __name__ == '__main__':
     app.run(
